da45f820f489df52d503feb2bcb34ceca32d65a9
TaskMasterErnest <kluernest08@gmail.com>
2023-08-01 13:12:06 +0000

    added a flag for counting bytes, and methods to execute byte counting

--- wordcounter/main.go
+++ wordcounter/main.go
@@ -11,17 +11,19 @@ import (
 func main() {
 	// define a boolean flag to count lines instead of words
 	lines := flag.Bool("l", false, "count lines")
+	// define a boolean flag to count bytes instead of words or lines
+	bites := flag.Bool("b", false, "count bytes")
 	// parse the command-line into the defined flag
 	flag.Parse()
 
 	// call the count function to count number of words
 	// count function takes input from Stdin and prints it out
 	// UPDATE: adding a pointer to take into account the result of the lines flag passed on the command-line
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bites))
 }
 
 // count function takes in a Reader and returns an int
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// splitting the words in a line of text
 	// call the bufio.ScanWords to do that
 	// UPDATE: by default the NewScanner defaults to ScanLines which scans a line of text till the \n delimiter.
@@ -33,12 +35,22 @@ func count(r io.Reader, countLines bool) int {
 		scanner.Split(bufio.ScanWords)
 	}
 
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}
+
 	// defining the wordcount
 	wordCount := 0
 
 	// scan the words and increment the scanner
-	for scanner.Scan() {
-		wordCount++
+	if countBytes {
+		for scanner.Scan() {
+			wordCount += len(scanner.Bytes())
+		}
+	} else {
+		for scanner.Scan() {
+			wordCount++
+		}
 	}
 
 	err := scanner.Err()
