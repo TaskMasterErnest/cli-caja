d63abc10307f6f271a219aeb85f3838f87a42540
TaskMasterErnest <kluernest08@gmail.com>
2023-08-01 11:54:55 +0000

    added a test case for counting lines, modified word count test to execute properly

--- wordcounter/main_test.go
+++ wordcounter/main_test.go
@@ -13,10 +13,27 @@ func TestCountWords(t *testing.T) {
 	numberOfBytes := 5
 
 	// call the count function to count the words in the buffer
-	result := count(bytesInBuffer)
+	// UPDATE: add false as input as response to the flag to be passed
+	result := count(bytesInBuffer, false)
 
 	// compare the results
 	if result != numberOfBytes {
-		t.Errorf("expected %d, got %d instead \n", numberOfBytes, result)
+		t.Errorf("expected %d, got %d instead.\n", numberOfBytes, result)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	// create a buffer to take in lines of text and store them as bytes
+	bytesInBuffer := bytes.NewBufferString("word1 word2 word3\nline1\nline2 word4")
+
+	// register the number of lines
+	numberOfLines := 3
+
+	// call the count function with the line flag passed to count number of lines in the buffer
+	result := count(bytesInBuffer, true)
+
+	// compare the results
+	if result != numberOfLines {
+		t.Errorf("expected %d, got %d instead.\n", numberOfLines, result)
 	}
 }
