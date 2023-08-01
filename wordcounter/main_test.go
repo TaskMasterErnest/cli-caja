ef244df8d5942d5e92904ed0754890b330099eaf
TaskMasterErnest <kluernest08@gmail.com>
2023-08-01 13:13:00 +0000

    added a test case for counting bytes, modified word and line counts to execute properly

--- wordcounter/main_test.go
+++ wordcounter/main_test.go
@@ -10,15 +10,15 @@ func TestCountWords(t *testing.T) {
 	bytesInBuffer := bytes.NewBufferString("word1 word2 word3 word4 word5\n")
 
 	// register the number of bytes in buffer
-	numberOfBytes := 5
+	numberOfWords := 5
 
 	// call the count function to count the words in the buffer
 	// UPDATE: add false as input as response to the flag to be passed
-	result := count(bytesInBuffer, false)
+	result := count(bytesInBuffer, false, false)
 
 	// compare the results
-	if result != numberOfBytes {
-		t.Errorf("expected %d, got %d instead.\n", numberOfBytes, result)
+	if result != numberOfWords {
+		t.Errorf("expected %d, got %d instead.\n", numberOfWords, result)
 	}
 }
 
@@ -30,10 +30,26 @@ func TestCountLines(t *testing.T) {
 	numberOfLines := 3
 
 	// call the count function with the line flag passed to count number of lines in the buffer
-	result := count(bytesInBuffer, true)
+	result := count(bytesInBuffer, true, false)
 
 	// compare the results
 	if result != numberOfLines {
 		t.Errorf("expected %d, got %d instead.\n", numberOfLines, result)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	// create a buffer to take in words and hold them as bytes
+	bytesInBuffer := bytes.NewBufferString("word1 word2 word3 word4 word5 word6")
+
+	// register the number of bytes
+	numberOfBytes := 35
+
+	// call the count function to count the number of bytes with the bytes flag
+	result := count(bytesInBuffer, false, true)
+
+	// compare the results
+	if result != numberOfBytes {
+		t.Errorf("expected %d, got %d instead.\n", numberOfBytes, result)
 	}
 }
