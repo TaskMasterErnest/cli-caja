ac7385e89aa05b1e0699506136d14dfca5b5b2ee
TaskMasterErnest <kluernest08@gmail.com>
2023-08-01 11:53:17 +0000

    added a flag to count lines

--- wordcounter/main.go
+++ wordcounter/main.go
@@ -2,23 +2,36 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
+	// define a boolean flag to count lines instead of words
+	lines := flag.Bool("l", false, "count lines")
+	// parse the command-line into the defined flag
+	flag.Parse()
+
 	// call the count function to count number of words
 	// count function takes input from Stdin and prints it out
-	fmt.Println(count(os.Stdin))
+	// UPDATE: adding a pointer to take into account the result of the lines flag passed on the command-line
+	fmt.Println(count(os.Stdin, *lines))
 }
 
 // count function takes in a Reader and returns an int
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// splitting the words in a line of text
 	// call the bufio.ScanWords to do that
+	// UPDATE: by default the NewScanner defaults to ScanLines which scans a line of text till the \n delimiter.
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+
+	// a conditional that switches between scanning words and scanning lines
+	// default is splitting by lines
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// defining the wordcount
 	wordCount := 0
